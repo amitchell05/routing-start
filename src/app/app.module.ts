1ba20d3fd530fcf4fdf317c8f0ea4d58615190c0
Angela Mitchell <anglarenmit@gmail.com>
2020-12-22 00:25:43 -0600

    Resolved dynamic data with the resolve guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { HomeComponent } from "./home/home.component";
-import { UsersComponent } from "./users/users.component";
-import { UserComponent } from "./users/user/user.component";
-import { ServersComponent } from "./servers/servers.component";
-import { ServersService } from "./servers/servers.service";
-import { ServerComponent } from "./servers/server/server.component";
-import { EditServerComponent } from "./servers/edit-server/edit-server.component";
-import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { AppRoutingModule } from "./app-routing.module";
-import { AuthGuard } from "./auth-guard.service";
-import { AuthService } from "./auth.service";
-import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServersService } from './servers/servers.service';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
+import { CanDeactivateGuard } from './servers/edit-server/can-deactivate-guard.service';
 import { ErrorPageComponent } from './error-page/error-page.component';
+import { ServerResolver } from './servers/server/server-resolver.service';
 
 @NgModule({
   declarations: [
@@ -27,10 +28,16 @@ import { ErrorPageComponent } from './error-page/error-page.component';
     EditServerComponent,
     ServerComponent,
     PageNotFoundComponent,
-    ErrorPageComponent,
+    ErrorPageComponent
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService, AuthService, AuthGuard, CanDeactivateGuard],
-  bootstrap: [AppComponent],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuard,
+    CanDeactivateGuard,
+    ServerResolver
+  ],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
